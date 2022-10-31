81f96ed66d0b9a6c6816d67bc1ded97e0f4969ac
maximgv <85558160+maximgv@users.noreply.github.com>
2022-10-31 21:59:44 +0000

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -68,6 +68,9 @@ ismoving = False
 ball_location = [50,50]
 ball_y_momentum = 0 
 air_timer = 0
+
+scroll = [0,0]
+
 ball_rect = pygame.Rect(50, 50, ball.get_width(),ball.get_height())
 barl_rect = pygame.Rect(100,100,100,50)
 ############## ################## ##########
@@ -77,18 +80,31 @@ barl_rect = pygame.Rect(100,100,100,50)
 while True: #the game loop 
     display.fill((146,244,255))
 
-    y=0
+
+    scroll[0] += (ball_rect.x-scroll[0]-156) /20
+    scroll[1] += (ball_rect.y-scroll[1]-106) /20
+
+    
+    pygame.draw.rect(display,(7,80,75),pygame.Rect(0,120,300,80))
+    for background_object in background_objects:
+        obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0],background_object[1][1]-scroll[1]*background_object[0],background_object[1][2],background_object[1][3])
+        if background_object[0] == 0.5:
+            pygame.draw.rect(display,(14,222,150),obj_rect)
+        else:
+            pygame.draw.rect(display,(9,91,85),obj_rect)
+    
+    
     tile_rects = []
+    y=0
     for row in game_map:            
         x=0
         for tile in row:                 #displays the tiles from the map
             if tile == '1':
-                display.blit(dirt, (x * TILE_SIZE, y * TILE_SIZE))
+                display.blit(dirt, (x * TILE_SIZE-scroll[0], y * TILE_SIZE-scroll[1]))
             if tile == '2':
-                display.blit(rock, (x * TILE_SIZE, y * TILE_SIZE))
+                display.blit(rock, (x * TILE_SIZE-scroll[0], y * TILE_SIZE-scroll[1]))
             if  tile != '0':
                 tile_rects.append(pygame.Rect(x*TILE_SIZE, y*TILE_SIZE,TILE_SIZE, TILE_SIZE))
-
             x+=1
         y +=1
 
@@ -115,7 +131,7 @@ while True: #the game loop
     else:
         air_timer += 1
 
-    display.blit(ball, (ball_rect.x, ball_rect.y))
+    display.blit(ball, (ball_rect.x-scroll[0], ball_rect.y-scroll[1]))
 
     for event in pygame.event.get(): # event loop
         if event.type == QUIT: # check for window quit
